cf874e97f8dd251167bf21fe21b00730edfab228
ramsys28 <mklados@gmail.com>
2017-04-12 20:28:24 +0200

    fixes of paths

--- eeglab14_0_0b/plugins/FCLAB1.0.0/fclab.m
+++ eeglab14_0_0b/plugins/FCLAB1.0.0/fclab.m
@@ -37,8 +37,10 @@ if nargin < 1
 	return;
 end
 
-%% Automatically download and add BCT, MITSE toolboxes in the plugin path
-general_folders = dir('plugins/FCLAB1.0.0');
+% Automatically download and add BCT, MITSE toolboxes in the plugin path
+eeglab_path=which('eeglab');
+eeglab_path=strrep(eeglab_path,'eeglab.m','');
+general_folders = dir([eeglab_path 'plugins/FCLAB1.0.0']);
 flag_BCT = 0;
 flag_MITSE = 0;
 
@@ -60,7 +62,7 @@ for k = length(general_folders):-1:1
     fname = general_folders(k).name;
     if (strcmp(fname, 'BCT') == 1)
         disp('>> FCLAB: BCT toolbox found!');
-        BCT_version = dir('plugins/FCLAB1.0.0/BCT');
+        BCT_version = dir([eeglab_path 'plugins/FCLAB1.0.0/BCT']);
         
         %find BCT version folder
         for j = length(BCT_version):-1:1
@@ -76,7 +78,7 @@ for k = length(general_folders):-1:1
         end
         
         disp('>> FCLAB: Adding to path...');
-        addpath(strcat('plugins/FCLAB1.0.0/BCT/', BCT_version.name));
+        addpath(strcat([eeglab_path 'plugins/FCLAB1.0.0/BCT/'], BCT_version.name));
         flag_BCT = 1;
         break;
     end
@@ -88,8 +90,8 @@ for k = length(general_folders):-1:1
     if (strcmp(fname, 'code') == 1)
         disp('>> FCLAB: MIT SE toolbox found!');
         disp('>> FCLAB: Adding to path...');
-        delete('plugins/FCLAB1.0.0/code/issymmetric.m'); %due to conflict with MATLAB's issymetric
-        addpath('plugins/FCLAB1.0.0/code');
+        delete([eeglab_path 'plugins/FCLAB1.0.0/code/issymmetric.m']); %due to conflict with MATLAB's issymetric
+        addpath([eeglab_path 'plugins/FCLAB1.0.0/code']);
         flag_MITSE = 1;
         break;
     end
@@ -99,12 +101,12 @@ end
 if(flag_BCT == 0)
     disp('>> FCLAB: BCT toolbox not found!');
     url = 'https://sites.google.com/site/bctnet/Home/functions/BCT.zip?attredirects=0';
-    filename = 'plugins/FCLAB1.0.0/BCT.zip';
+    filename = [eeglab_path 'plugins/FCLAB1.0.0/BCT.zip'];
     disp(['>> FCLAB: Downloading BCT toolbox from: ', url]);
     websave(filename, url);
     disp('>> FCLAB: Unzipping...');
-    unzip('plugins/FCLAB1.0.0/BCT.zip', 'plugins/FCLAB1.0.0');
-    folders = dir('plugins/FCLAB1.0.0/BCT');
+    unzip([eeglab_path 'plugins/FCLAB1.0.0/BCT.zip'], [eeglab_path 'plugins/FCLAB1.0.0/']);
+    folders = dir([eeglab_path 'plugins/FCLAB1.0.0/BCT']);
     for j = length(folders):-1:1
         if (~folders(j).isdir)
             folders(j) = [];
@@ -117,23 +119,23 @@ if(flag_BCT == 0)
         end
     end
     disp('>> FCLAB: Adding to path...');
-    addpath(strcat('plugins/FCLAB1.0.0/BCT/', folders.name));
-    delete('plugins/FCLAB1.0.0/BCT.zip');
+    addpath(strcat([eeglab_path 'plugins/FCLAB1.0.0/BCT/'], folders.name));
+    delete([eeglab_path 'plugins/FCLAB1.0.0/BCT.zip']);
     disp('>> FCLAB: Done...');
 end
 
 %MITSE doesn't exist - download from site and automatically unzip
 if(flag_MITSE == 0)
     url = 'http://strategic.mit.edu/docs/matlab_networks/matlab_networks_routines.zip';
-    filename = 'plugins/FCLAB1.0.0/matlab_networks_routines.zip';
+    filename = [eeglab_path 'plugins/FCLAB1.0.0/matlab_networks_routines.zip'];
     disp(' '); disp(['>> FCLAB: Downloading MIT Strategic Engineering toolbox from: ', url]);
     websave(filename, url);
     disp('>> FCLAB: Unzipping...');
-    unzip('plugins/FCLAB1.0.0/matlab_networks_routines.zip', 'plugins/FCLAB1.0.0');
-    delete('plugins/FCLAB1.0.0/code/issymmetric.m'); %due to conflict with MATLAB's issymetric
+    unzip([eeglab_path 'plugins/FCLAB1.0.0/matlab_networks_routines.zip'], [eeglab_path 'plugins/FCLAB1.0.0/']);
+    delete([eeglab_path 'plugins/FCLAB1.0.0/code/issymmetric.m']); %due to conflict with MATLAB's issymetric
     disp('>> FCLAB: Adding to path...');
-    addpath('plugins/FCLAB1.0.0/code');
-    delete('plugins/FCLAB1.0.0/matlab_networks_routines.zip');
+    addpath([eeglab_path 'plugins/FCLAB1.0.0/code']);
+    delete([eeglab_path 'plugins/FCLAB1.0.0/matlab_networks_routines.zip']);
     disp('>> FCLAB: Done...');
 end
 
@@ -181,7 +183,7 @@ if strcmp(params.metric, 'cor');
             else
                 temp_adj = corrcoef(mean(testEEG.data,3)'); % events data
             end
-            eval(['outEEG.FC.Correlation.' params.bands{i,2} '.adj_matrix=temp_adj']);
+            eval(['outEEG.FC.Correlation.' params.bands{i,2} '.adj_matrix=temp_adj;']);
             
             if params.graph==1
                 L = weight_conversion(temp_adj, 'lengths'); %connection-length matrix
@@ -197,7 +199,7 @@ if strcmp(params.metric, 'cor');
                 %global measures
                 eval(['[~, ~, ~, ~, outEEG.FC.Correlation.'  params.bands{i,2} '.global.diam] = charpath(D);']);
                 eval(['[~, ~, ~, outEEG.FC.Correlation.'  params.bands{i,2} '.global.rad, ~] = charpath(D);']);
-                eval(['outEEG.FC.Correlation.'  params.bands{i,2} '.global.LN = leafNodes(temp_adj);']);
+                eval(['outEEG.FC.Correlation.'  params.bands{i,2} '.global.LN = leaf_nodes(temp_adj);']);
                 eval(['[outEEG.FC.Correlation.'  params.bands{i,2} '.global.lambda, ~, ~, ~, ~] = charpath(D);']);
                 %outEEG.FC.Correlation.global.DEGcor = pearson(temp_adj); %CHECK
                 %THIS --> this is fot weighted
@@ -210,29 +212,6 @@ if strcmp(params.metric, 'cor');
     end;
 end
 
-disp(' ');
-disp('>> FCLAB: Automatically discarding potential negative values...');
-temp_adj(temp_adj < 0) = 0;
-outEEG.FC.correlation.adj_matrix = temp_adj;
-
-L = weight_conversion(temp_adj, 'lengths'); %connection-length matrix
-D = distance_wei(L); %distance matrix
-
-disp('>> FCLAB: Computing local metrics -> BC, DEG, ECC, clustering coefficient, local efficiency, eigenvalue centrality...');
-outEEG.FC.correlation.local.BC = betweenness_wei(L)./((n-1)*(n-2));
-outEEG.FC.correlation.local.DEG = degrees_und(temp_adj)./(n-1);
-[~, ~, outEEG.FC.correlation.local.ECC, ~, ~] = charpath(D);
-outEEG.FC.correlation.local.clustcoef = clustering_coef_wu(temp_adj);
-outEEG.FC.correlation.local.Elocal = efficiency_wei(temp_adj, 1); %or 2 for modified version
-outEEG.FC.correlation.local.EC = eigenvector_centrality_und(temp_adj);
-
-disp('>> FCLAB: Computing global metrics -> diameter, radius, number of leaves, lambda, degree correlation, global efficiency...');
-[~, ~, ~, ~, outEEG.FC.correlation.global.diam] = charpath(D);
-[~, ~, ~, outEEG.FC.correlation.global.rad, ~] = charpath(D);
-outEEG.FC.correlation.global.LN = leaf_nodes(temp_adj);
-[outEEG.FC.correlation.global.lambda, ~, ~, ~, ~] = charpath(D);
-outEEG.FC.correlation.global.DEGcor = pearson(temp_adj); %CHECK THIS
-outEEG.FC.correlation.global.Eglobal = efficiency_wei(temp_adj, 0);
 
 disp('>> FCLAB: Done!');
 
