0359c0994d95b564d1a54db24b520df525d47f6b
Bill Pezoulas <bPezoulas@gmail.com>
2017-05-10 14:29:20 +0300

    One extra input for band identifier

--- eeglab14_0_0b/plugins/FCLAB1.0.0/fclab_MST.m
+++ eeglab14_0_0b/plugins/FCLAB1.0.0/fclab_MST.m
@@ -1,4 +1,4 @@
-function [MST_net, MST_params]=fclab_MST(Matrix)
+function [MST_net, MST_params]=fclab_MST(Matrix, band_ID)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % [MST_net, MST_params] = fclab_MST(Matrix)
 %
@@ -18,7 +18,8 @@ addpath('MST_params');
 Matrix = double(Matrix);
 n = size(Matrix, 2);
 Matrix(Matrix<0) = 0; %retain only positive values (for sure)
-    
+
+tic;
 [MST_net.Kruskal, MST_net.cost] =  minSpanTreeKruskal(1./Matrix);
 [MST_net.links, MST_net.weights] = minimal_spanning_tree(1./Matrix);
 
@@ -41,5 +42,8 @@ MST_params.leaf_fraction = max(size(leaf_nodes(MST_net.Kruskal)))/(n-1);
 MST_params.Th = treeHierarchy(MST_params.leaves, n-1, max(MST_params.BC));
 MST_params.DEGcor = pearson(MST_net.Kruskal);
 MST_params.kappa = kappa(MST_params.DEG);
+timer = toc;
+disp(['>> fclab_MST: Elapsed time for ', band_ID, ' band = ', num2str(timer), ' sec']);
+disp(' ');
 
 return;
