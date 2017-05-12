cbd5a2e658b923e5801beadf02f6be29aee7ec15
Bill Pezoulas <bPezoulas@gmail.com>
2017-05-12 15:32:13 +0300

    Added a short description for the metric

--- eeglab14_0_0b/plugins/FCLAB1.0.0/FC_metrics/fcmetric_mutualinf.m
+++ eeglab14_0_0b/plugins/FCLAB1.0.0/FC_metrics/fcmetric_mutualinf.m
@@ -1,17 +1,42 @@
-function outEEG=fcmetric_mutualinf(inEEG)
-w = warning('query','last')
+function outEEG = fcmetric_mutualinf(inEEG)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Usage:
+%
+%   >>  outEEG = fcmetric_mutualinf(inEEG);
+%
+% Inputs:
+%           inEEG   - input EEG dataset
+%   
+%    
+% Outputs:
+%           outEEG  - output dataset
+%
+% Info:
+%           Computes the mutual information for each possible pair of EEG 
+%           channels and for every band as well.
+%
+% Mathematical background:
+%
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+w = warning('query','last');
+outEEG = inEEG;
 id = w.identifier;
-warning('off',id)
-mf = size(inEEG.FC.parameters.bands,1);
-for bands=1:mf
-    freq_range=str2num(inEEG.FC.parameters.bands{bands,1});
-    for i=1:inEEG.nbchan-1
-        for j=i+1:inEEG.nbchan
-           Matrix(i,j)=mutualinf(inEEG.data(i,:)',inEEG.data(j,:)',inEEG.srate,freq_range(1),freq_range(2));
-        end;
-    end;
-    Matrix=Matrix+Matrix';
-     eval(['outEEG.FC.mutualinf.' strrep(inEEG.FC.parameters.bands{i,2},' ','_') '.adj_matrix=Matrix;']);
-end;
+warning('off',id);
+mf = size(inEEG.FC.parameters.bands, 1);
+disp('>> FCLAB: Mututal information is being computed...');
+
+for bands = 1:mf
+    freq_range = str2num(inEEG.FC.parameters.bands{bands,1});
+    for i = 1:inEEG.nbchan-1
+        for j = i+1:inEEG.nbchan
+           Matrix(i,j) = mutualinf(inEEG.data(i,:)',inEEG.data(j,:)',inEEG.srate,freq_range(1),freq_range(2));
+        end
+    end
+    Matrix = Matrix + Matrix';
+    eval(['outEEG.FC.mutualinf.' strrep(inEEG.FC.parameters.bands{i,2},' ','_') '.adj_matrix=Matrix;']);
+end
             
        
