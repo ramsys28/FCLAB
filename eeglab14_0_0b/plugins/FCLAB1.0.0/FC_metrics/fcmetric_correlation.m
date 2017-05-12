5dfe6779c2bea1cd58394d041145c0a96562cf64
Bill Pezoulas <bPezoulas@gmail.com>
2017-05-12 15:32:52 +0300

    Added a short description fot the metric

--- eeglab14_0_0b/plugins/FCLAB1.0.0/FC_metrics/fcmetric_correlation.m
+++ eeglab14_0_0b/plugins/FCLAB1.0.0/FC_metrics/fcmetric_correlation.m
@@ -1,10 +1,30 @@
-function outEEG=fcmetric_correlation(inEEG)
-mf = size(inEEG.FC.parameters.bands,1);
+function outEEG = fcmetric_correlation(inEEG)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Usage:
+%
+%   >>  outEEG = fcmetric_correlation(inEEG);
+%
+% Inputs:
+%           inEEG  - input EEG dataset
+%   
+%    
+% Outputs:
+%           outEEG  - output dataset
+%
+% Info:
+%           Computes Pearson's temporal correlation coefficient for each 
+%           possible pair of EEG channels and for every band as well.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+mf = size(inEEG.FC.parameters.bands, 1);
 [m, n, o] = size(inEEG.data);
-outEEG=inEEG;
+outEEG = inEEG;
+disp('>> FCLAB: Pearson''s correlation is being computed...');
+
 for i = 1:mf
     testEEG = inEEG;
-    freq_range=str2num(inEEG.FC.parameters.bands{i,1});
+    freq_range = str2num(inEEG.FC.parameters.bands{i,1});
     [testEEG, com, b] = pop_eegfiltnew(testEEG, freq_range(1));
     [testEEG, com, b] = pop_eegfiltnew(testEEG, [],freq_range(2));
     if (o == 1)
@@ -14,5 +34,3 @@ for i = 1:mf
     end
     eval(['outEEG.FC.correlation.' strrep(inEEG.FC.parameters.bands{i,2},' ','_') '.adj_matrix=temp_adj;']);
 end;
-
-    
