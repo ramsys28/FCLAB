896d6d755a44dd3ae14eec9777f84974c3dcb7e6
Bill Pezoulas <bPezoulas@gmail.com>
2017-05-12 15:30:23 +0300

    Added full description of the metric

--- eeglab14_0_0b/plugins/FCLAB1.0.0/FC_metrics/fcmetric_PLI.m
+++ eeglab14_0_0b/plugins/FCLAB1.0.0/FC_metrics/fcmetric_PLI.m
@@ -1,9 +1,67 @@
 function outEEG = fcmetric_PLI(inEEG)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Usage:
+%
+%   >>  outEEG = fcmetric_PLI(inEEG);
+%
+% Inputs:
+%           inEEG   - input EEG dataset
+%   
+%    
+% Outputs:
+%           outEEG  - output dataset
+%
+% Info:
+%           Computes the phase lag index proposed by Stam et al., 2007 for 
+%           each possible pair of EEG channels and for every band as well.
+%
+% Mathematical background:
+%
+%           According to Stam et al., 2007 PLI is defined in a complete 
+%           mathematical formula as:
+%
+%                   PLI = mean(|sign([sin(Df(t))])|)        (1)
+%
+%           where:
+%
+%                   Df(t) = phase1(t)-phase2(t)), is the phase difference
+%                   of the two signals at time t, 
+%
+%                   phase1(t) is the phase of the 1st signal and is equal
+%                   to arctan(x1_H(t)/x1(t)) where x1_H(t) is the Hilbert
+%                   transformed version of signal x1(t),
+%
+%                   phase2(t) is the phase of the 2nd signal and is equal
+%                   to arctan(x2_H(t)/x2(t)) where x2_H(t) is the Hilbert
+%                   transformed version of signal x2(t).
+%
+% Fundamental basis:
+%
+%               The PLI ranges between 0 and 1. 
+%               A PLI of zero indicates either no coupling or coupling with 
+%               a phase difference centered around 0 mod p. A PLI of 1 
+%               indicates perfect phase locking at a value of Df different 
+%               from 0 mod p. The stronger this nonzero phase locking is, 
+%               the larger PLI will be.
+%           
+% Important notes:
+%
+%               the sinus in (1) is used in order to convert the phases on 
+%               the interval (0, 2pi] instead of (-pi, pi],
+%
+%               the mean value is used as an alternative way in order to
+%               highlight the fact that when the distibution of Df is 
+%               symmetric then the two signals originate from the same
+%               source. The same happens when the mean Df is equal to or 
+%               centered around 0 mod pi.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 mf = size(inEEG.FC.parameters.bands, 1);
 [m, ~, o] = size(inEEG.data);
 outEEG = inEEG;
 temp_adj = zeros(m, m);
+disp('>> FCLAB: PLI is being computed...');
 
 for i = 1:mf
     testEEG = inEEG;
