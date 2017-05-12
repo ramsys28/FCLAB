52d2d687fc29a511c43043f1832d87d8ed38c03a
Bill Pezoulas <bPezoulas@gmail.com>
2017-05-12 15:38:08 +0300

    Added a short description of the metric

--- eeglab14_0_0b/plugins/FCLAB1.0.0/FC_metrics/fcmetric_MSC.m
+++ eeglab14_0_0b/plugins/FCLAB1.0.0/FC_metrics/fcmetric_MSC.m
@@ -26,7 +26,7 @@ mf = size(inEEG.FC.parameters.bands,1);
 outEEG = inEEG;
 disp('>> FCLAB: MSC is being computed...');
 
-for i1=1:m1
+for i1 = 1:m1
     i1
     for j1=i1:m1
         [Cxy F]=mscohere(inEEG.data(i1,:),inEEG.data(j1,:),50,1,[],inEEG.srate);
@@ -37,22 +37,22 @@ for i1=1:m1
                 if F(i,1)>freq_range(1)
                     start_freq=i;
                     break;
-                end;
-            end;
+                end
+            end
             for i=1:length(F)
                 if F(i,1)>freq_range(2)
                     stop_freq=i-1;
                     break;
-                end;
-            end;
-        end;
+                end
+            end
+        end
          eval(['outEEG.FC.MSC.'...
              strrep(inEEG.FC.parameters.bands{bands,2},' ','_') ...
              '.adj_matrix(i1,j1)=mean(Cxy(start_freq:stop_freq-1,1));']);     
-    end;
-end;
+    end
+end
 
-for i=1:mf
+for i = 1:mf
 eval(['outEEG.FC.MSC.'...
              strrep(inEEG.FC.parameters.bands{i,2},' ','_') ...
              '.adj_matrix(outEEG.nbchan,:)=0;']);
@@ -63,4 +63,4 @@ eval(['outEEG.FC.MSC.'...
              '.adj_matrix+outEEG.FC.MSC.'...
              strrep(inEEG.FC.parameters.bands{i,2},' ','_') ...
              '.adj_matrix''']);
-end;
+end
