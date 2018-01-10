c3ee49ad4df2f8b82bc6707dfdc87a39f2059baf
Bill Pezoulas <bPezoulas@gmail.com>
2018-01-10 13:03:15 +0200

    Add files via upload

--- FCLAB1.0.0/eegplugin_fclab.m
+++ FCLAB1.0.0/eegplugin_fclab.m
@@ -11,12 +11,14 @@ comfc = [try_strings.no_check 'EEG = pop_fclab(EEG); [ALLEEG EEG CURRENTSET] = e
 vizfc = [try_strings.no_check 'pop_fcvisual(EEG);' catch_strings.new_and_hist];
 graphfc = [try_strings.no_check 'EEG = pop_fcgraph(EEG);' catch_strings.new_and_hist];
 graphparamvizfc = [try_strings.no_check 'pop_fcvisual_parameters(EEG);' catch_strings.new_and_hist];
-submenu = uimenu( menu, 'Label', 'FCLAB', 'separator', 'on');
+vizmst = [try_strings.no_check 'pop_fcvisual_MSTs(EEG);' catch_strings.new_and_hist];
+submenu = uimenu(menu, 'Label', 'FCLAB', 'separator', 'on');
 
-uimenu( submenu, 'label', 'Compute Functional Connectivity', 'callback',comfc );
+uimenu(submenu, 'label', 'Compute Functional Connectivity', 'callback', comfc);
 % uimenu( submenu, 'label', 'Visualize Functional Connectivity', 'callback', vizfc, 'userdata','chanloc:off');
-uimenu( submenu, 'label', 'Visualize Functional Connectivity', 'callback', vizfc);
-uimenu( submenu, 'label', 'Graph/MST analysis', 'callback', graphfc);
-uimenu( submenu, 'label', 'Visualize Graph/MST parameters', 'callback', graphparamvizfc);
+uimenu(submenu, 'label', 'Visualize Functional Connectivity', 'callback', vizfc);
+uimenu(submenu, 'label', 'Graph/MST analysis', 'callback', graphfc);
+uimenu(submenu, 'label', 'Visualize Graph/MST parameters', 'callback', graphparamvizfc);
+uimenu(submenu, 'label', 'Visualize MST', 'callback', vizmst);
  
 return
